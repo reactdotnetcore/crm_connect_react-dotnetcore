87b633654808085f7a9a26ad6fea3b7107a3e956
Aakash Gupta <guptaaakash333@gmail.com>
2024-11-13 16:54:45 +0530

    Revert "footer Added"
    This reverts commit 3a30e5ed09ee52b6477d27050e5bd3091537698d.

--- src/ThemeLayout/Footer.tsx
+++ src/ThemeLayout/Footer.tsx
@@ -2,19 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <>
-    <footer id="footer" className="footer">
-      <div className="copyright">
-        &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-      </div>
-      <div className="credits">
-        {/* All the links in the footer should remain intact. */}
-        {/* You can delete the links only if you purchased the pro version. */}
-        {/* Licensing information: https://bootstrapmade.com/license/ */}
-        {/* Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ */}
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </footer>
-    </>
+    <div>Footer</div>
   )
 }
