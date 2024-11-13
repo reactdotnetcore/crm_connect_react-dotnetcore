8eac6bbe87f76af578fb853ffebae23d2fd8b84b
Swastik021 <vu1s2021021@pvppcoe.ac.in>
2024-11-13 15:27:37 +0530

    sidebar added

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './ThemeLayout/Header'
+import Sidebar from './ThemeLayout/Sidebar'
+import Footer from './ThemeLayout/Footer'
 
 export default function Layout() {
   return (
    <>
     <Header/>
+    <Sidebar/>
+    <Footer/>
    </>
   )
 }
