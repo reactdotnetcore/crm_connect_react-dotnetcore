97d2231bd726a2e7b7da3fa39716c9ac9fe77fb0
Swastik021 <vu1s2021021@pvppcoe.ac.in>
2024-11-14 11:08:17 +0530

    Registration added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Layout from './Layout';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Registration from './Authentication/Registration';
 
-function App() {
-  return (
-    <div className="App">
-      <Layout/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={Layout} />
+            <Route Component={Layout} >
+              <Route path="Sign" Component={Registration} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './ThemeLayout/Header'
 import Sidebar from './ThemeLayout/Sidebar'
 import Footer from './ThemeLayout/Footer'
+import { Outlet } from 'react-router'
 
 export default function Layout() {
   return (
    <>
     <Header/>
     <Sidebar/>
+    <Outlet/>
     <Footer/>
    </>
   )
