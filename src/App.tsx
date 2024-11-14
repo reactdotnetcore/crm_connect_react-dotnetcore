f436edf39df2b33effacfce125d92ac683ca485b
Swastik021 <vu1s2021021@pvppcoe.ac.in>
2024-11-14 11:12:06 +0530

    Revert "Merge branch 'master' of https://github.com/reactdotnetcore/crm_connect_react-dotnetcore"
    This reverts commit b5e0e5d4e90d26f2c45711e09f13b9a3bb2bdc2d, reversing
    changes made to b1097748f988deb49d9341a5100e6bff90b9b4f6.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,13 @@
-import React, { Children, useEffect, useState } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate  } from 'react-router-dom';
 import Layout from './Layout';
-import Login from './Authentication/Login';
-import { resolve } from 'path';
-
-interface ProtectedRouteProps {
-  isAuthenticated: boolean;
-  children: JSX.Element;
-}
 
 function App() {
-
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(()=>{
-    const checkAuthStatus = async () =>{
-      const authStatus = await fakeAuthStatus();
-      setIsAuthenticated(authStatus);
-    };
-    checkAuthStatus();
-  },[])
-
-  const fakeAuthStatus = async () =>{
-    return new Promise<boolean>((resolve) => setTimeout(() =>resolve(true),1000));
-  }
-  const ProtectedRoute = ({isAuthenticated, children}: ProtectedRouteProps) =>{
-    return isAuthenticated ? children : <Navigate to="/login"/>
-  }
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login/>} />
-          {/* <Route path="/register" element={<Register />} /> */}
-          <Route path="/" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Layout /></ProtectedRoute>} />
-        </Routes>
-      </Router>
-    </>
+    <div className="App">
+      <Layout/>
+    </div>
   );
 }
 
