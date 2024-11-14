b1097748f988deb49d9341a5100e6bff90b9b4f6
Swastik021 <vu1s2021021@pvppcoe.ac.in>
2024-11-14 11:10:17 +0530

    Revert "Registration added"
    This reverts commit 97d2231bd726a2e7b7da3fa39716c9ac9fe77fb0.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Layout from './Layout';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Registration from './Authentication/Registration';
 
-class App extends Component {
-  render() {
-    return (
-      <div >
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" Component={Layout} />
-            <Route Component={Layout} >
-              <Route path="Sign" Component={Registration} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Layout/>
+    </div>
+  );
 }
 
 export default App;
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import Header from './ThemeLayout/Header'
 import Sidebar from './ThemeLayout/Sidebar'
 import Footer from './ThemeLayout/Footer'
-import { Outlet } from 'react-router'
 
 export default function Layout() {
   return (
    <>
     <Header/>
     <Sidebar/>
-    <Outlet/>
     <Footer/>
    </>
   )
