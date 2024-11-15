5b011a94fdd30409d7401f95dc278dc84b8b770d
Aakash Gupta <guptaaakash333@gmail.com>
2024-11-15 15:55:24 +0530

    Login Completed

--- src/Authentication/Login.tsx
+++ src/Authentication/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useFormik } from 'formik'
-import * as Yop from 'yup'
+import { useFormik } from 'formik';
+import * as Yop from 'yup';
 //import logo from 'public/assets/img/logo.png'
 
 interface LoginForm {
